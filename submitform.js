36166f0cc12e88e879f4a22d007eade1e6c272e9
Eduardo Mejia <mejiaeduardo@yahoo.com>
2024-10-23 23:18:21 -0400

    fix validation

--- submitform.js
+++ submitform.js
@@ -1,3 +1,22 @@
 const projectChargeCode = document.querySelector("#project-charge-code");
+const errorChargeCode = document.querySelector("#error-charge-code");
+const btn = document.querySelector("button");
 
-const regex = /[EIM]/;
+//  Add click event to button
+
+function validateProjChargeCode(str) {
+  // Instantiate regex object to check that project charge
+  // code begins with either 'E, 'I', or 'M'.
+
+  const regex = /[EIM]/;
+  const correctChargeCode = new RegExp(regex);
+
+  const result = str.match(correctChargeCode);
+
+  if (!result) {
+    errorChargeCode.color = "red";
+    errorChargeCode.innerHTML = `Project charge code must begin with either 'E, 'I', or 'M'`;
+  } else {
+    errorChargeCode.innerHTML = "Good to go";
+  }
+}
